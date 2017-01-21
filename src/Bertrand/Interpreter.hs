7a21b0eccd025bc8e2d1c05639cf8ef2ca8f0d2d
Yuuki Fujihara <yuuki.fj@Yuukis-MacBook-Pro.local>
2017-01-21 23:01:35 +0900

    add lambdaTree

--- src/Bertrand/Interpreter.hs
+++ src/Bertrand/Interpreter.hs
@@ -6,6 +6,7 @@ module Bertrand.Interpreter
 
 import Prelude hiding (read)
 import Control.Monad
+import Control.Monad.Extra
 import Control.Monad.State
 import Data.Either
 import Data.List
@@ -13,6 +14,7 @@ import qualified Data.Map as Map
 import Data.Maybe
 import Data.Monoid
 import Data.Tree
+import Data.Tuple
 
 import Bertrand.Data
 
@@ -44,7 +46,7 @@ binds    (Envir _ _ bs) = bs
 
 type Interpreter a b = a -> State (Memory Envir) b
 
-type Evaluator = Ref -> Interpreter Expr Expr
+type Evaluator a b = Ref -> Interpreter a b
 
 interpret :: Interpreter a b -> Memory Envir -> a -> b
 interpret f s a = let (b, s') = runState (f a) s
@@ -97,7 +99,7 @@ evalShow = let (m, ref) = singleton Root
            --
 
 
-envir :: Ref -> Interpreter Expr Expr
+envir :: Evaluator Expr Expr
 envir r (Decl [] a) = envir r a
 envir r (Decl ds a) = do
     let (bs, es) = partition (\x -> case x of Bind _ _ -> True
@@ -112,7 +114,7 @@ envir r a = emapM (envir r) a
 toBinds :: [Expr] -> Binds
 toBinds = foldr (\(Bind s a) -> Map.insertWith (++) s [a]) Map.empty
 
-evalStep :: Evaluator
+evalStep :: Evaluator Expr Expr
 -- eval r (Var s)
 evalStep r e = case e of
     -- Var s     -> commaWith e . concatMap (Map.findWithDefault [] s . binds) <$> envirs r
@@ -123,7 +125,8 @@ evalStep r e = case e of
     App a b -> do
          as <- toList <$> evalTillLambda r a
          let cs = map (`appCons` b) (filter isCons as)
-         ls    <- applyTree r (lambdaTree $ filter isLambda as , b)
+         fs <- lambdaTree r $ filter isLambda as
+         ls <- applyTree r (fs, b)
         --  ls    <- mapM (\l -> apply r (l, b)) (filter isLambda as)
          return $ Comma $ cs ++ ls
         --  return $ Comma $ map (`appCons` b)   (filter isCons as)
@@ -177,12 +180,58 @@ evalStep r e = case e of
                       in  Cons s $ map f as ++ [b]
         -- appCons (Cons s es) e = Cons s $ es ++ [e]
 
-        lambdaTree :: [Expr] -> Forest Expr
-        -- lambdaTree = map (`Node` [])
-        lambdaTree es = let as = map (`Node` []) es
-                        in trace (drawForest $ map (fmap show) as) as
+        lambdaTree :: Evaluator [Expr] (Forest Expr)
+        lambdaTree r s = (\as -> trace (drawForest $ map (fmap show) as) as)
+                         <$> foldM (curry $ addForest r) [] s
+            where
+                addForest :: Evaluator (Forest Expr, Expr) (Forest Expr)
+                -- f :: Forest Expr -> Expr -> State (Memory Envir) (Forest Expr)
+                addForest r (ts, e) = let
+                    (re, e') = detachRef r e
+                    in case e' of
+                        Lambda a _ -> do
+                            -- a == b, a /= b, a => b, a <= b
+                            (xs, ys, zs, ws) <- foldM
+                                (\(xs, ys, zs, ws) t@(Node x _) ->
+                                    let (rx, Lambda b _) = detachRef r x
+                                        a' = Env re a
+                                        b' = Env rx b
+                                    in do
+                                        aimpb <- isJust <$> implicate r (a', b')
+                                        bimpa <- isJust <$> implicate r (b', a')
+                                        return $ case (aimpb, bimpa) of
+                                            (True,  True)  -> (t:xs, ys, zs, ws)
+                                            (False, False) -> (xs, t:ys, zs, ws)
+                                            (True,  False) -> (xs, ys, t:zs, ws)
+                                            (False, True)  -> (xs, ys, zs, t:ws)
+                                ) ([], [], [], []) ts
+                            if not $ null xs
+                            then let Node _ xs' = head xs
+                                 in return $ Node e xs' : ts
+                            else if not $ null zs
+                            then (++ ys) <$> mapM (\(Node z zs') -> Node z <$> addForest r (zs', e)) zs
+                            else return $ Node e ws : ys
 
-        applyTree :: Ref -> Interpreter (Forest Expr, Expr) [Expr]
+                            --
+                            --
+                            -- (xs, ys) <- partitionM (\(Node x _) ->
+                            --     let (rx, Lambda b _) = detachRef r x
+                            --     in isJust <$> implicate r (Env re a, Env rx b)) ts
+                            -- if null xs
+                            -- then return $ Node e [] : ts
+                            -- else (++ ys) <$> mapM (\(Node x ts') -> Node x <$> f ts' e) xs
+                        _ -> return ts
+
+        -- lambdaTree' :: [Expr] -> Forest Expr
+        -- lambdaTree = map (`Node` [])
+        -- lambdaTree es = let as = map (`Node` []) es
+        --                 in trace (drawForest $ map (fmap show) as) as
+        -- lambdaTree' = foldr (\e es -> Node e []:es) []
+        --     where
+        --         f :: Expr -> Forest Expr -> Forest Expr
+        --         f = undefined
+
+        applyTree :: Evaluator (Forest Expr, Expr) [Expr]
         applyTree r (xs, e) = concat <$> mapM
             (\(Node a bs) -> apply r (a, e) >>=
                              maybe (return [])
@@ -197,7 +246,7 @@ evalStep r e = case e of
                 defaultl a [] = [a]
                 defaultl _ as = as
 
-        apply :: Ref -> Interpreter (Expr, Expr) (Maybe Expr)
+        apply :: Evaluator (Expr, Expr) (Maybe Expr)
         -- apply r (a, b) = undefined
         -- apply :: Ref -> Interpreter (Expr, Expr) Expr
         apply r (a, b) = case a of
@@ -205,9 +254,9 @@ evalStep r e = case e of
             -- Lambda c d -> return (Just d)
             Lambda c d -> fmap (maybe d (`Env` d)) <$> match r (c, b)
 
-        match :: Ref -> Interpreter (Expr, Expr) (Maybe (Maybe Ref))
+        match :: Evaluator (Expr, Expr) (Maybe (Maybe Ref))
         match r t = do
-            m <- match' r t
+            m <- implicate r (swap t)
             maybe (return Nothing) (\es ->
                 case es of
                     [] -> return $ Just Nothing
@@ -216,59 +265,57 @@ evalStep r e = case e of
                         return $ Just $ Just r'
                   ) m
 
-            where
-                match' :: Ref -> Interpreter (Expr, Expr) (Maybe [Expr])
-                match' r t = let
-                    (a, b) = t
-                    (ra, a') = detachRef r a
-                    -- (rb, b') = detachRef r b
-                    in case (a', b) of
-                    (App _ _, _) -> do
-                        (ra', a'') <- detachRef r <$> evalTillTerm r a
-                        case a'' of
-                            App _ _ -> return Nothing
-                            _       -> match' r (a'', b)
+        implicate :: Evaluator (Expr, Expr) (Maybe [Expr])
+        implicate r t = let
+            (a, b) = t
+            (ra, a') = detachRef r a
+            (rb, b') = detachRef r b
+            in case (a', b') of
+            (_, App _ _) -> do
+                (rb', b'') <- detachRef r <$> evalTillTerm r b
+                case b'' of
+                    App _ _ -> return Nothing
+                    _       -> implicate r (b'', a)
 
-                    (Cons xs as, _) -> do
-                        (rb, b') <- detachRef r <$> evalTillCons r b
-                        case b' of
-                            Cons ys bs | xs == ys && length as == length bs -> do
-                                ms <- mapM (match' r) $ zip (map (Env ra) as) (map (Env rb) bs)
-                                if Nothing `elem` ms
-                                then return Nothing
-                                else return $ Just $ concat $ catMaybes ms
-                            _ -> return Nothing
+            (_, Cons xs bs) -> do
+                (ra', a'') <- detachRef r <$> evalTillCons r a
+                case a'' of
+                    Cons ys as | xs == ys && length as == length bs -> do
+                        ms <- mapM (implicate r) $ zip (map (Env rb) bs) (map (Env ra') as)
+                        if Nothing `elem` ms
+                        then return Nothing
+                        else return $ Just $ concat $ catMaybes ms
+                    _ -> return Nothing
 
-                    (Bytes i, Bytes j) | i == j -> return $ Just []
+            (Bytes i, Bytes j) | i == j -> return $ Just []
 
-                    (Comma as, _) -> do
-                        ms <- mapM (match' r . (, b) . Env ra) as
-                        if any isJust ms
-                        then return $ Just $ concat $ catMaybes ms
-                        else return Nothing
+            (_, Comma bs) -> do
+                ms <- mapM (implicate r . (, a) . Env rb) bs
+                if any isJust ms
+                then return $ Just $ concat $ catMaybes ms
+                else return Nothing
 
-                    (Var s, _) -> return $ Just [Bind s b]
+            (_, Var s) -> return $ Just [Bind s a]
 
+            (x, y) -> trace ("failed:" ++ show (x, y)) return Nothing
 
-                    (a, b) -> trace "failed" return Nothing
+        evalTillTerm :: Evaluator Expr Expr
+        evalTillTerm r a = evalStep r a >>=
+            (\a' -> if a == a'
+                    then return a
+                    else case snd $ detachRef r a' of
+                        App _ _ -> evalTillTerm r a'
+                        _       -> return a )
 
-                evalTillTerm :: Evaluator
-                evalTillTerm r a = evalStep r a >>=
-                    (\a' -> if a == a'
-                            then return a
-                            else case snd $ detachRef r a' of
-                                App _ _ -> evalTillTerm r a'
-                                _       -> return a )
-
-                evalTillCons :: Evaluator
-                evalTillCons r a = evalStep r a >>=
-                    (\a' -> let (r', b) = detachRef r a' in case b of
-                        Cons _ _    -> return a'
-                        Comma as    -> do
-                            as' <- mapM (evalTillLambda r') as
-                            return $ Env r' $ Comma as'
-                        _ | a == a' -> return a
-                        _           -> evalTillCons r a')
+        evalTillCons :: Evaluator Expr Expr
+        evalTillCons r a = evalStep r a >>=
+            (\a' -> let (r', b) = detachRef r a' in case b of
+                Cons _ _    -> return a'
+                Comma as    -> do
+                    as' <- mapM (evalTillLambda r') as
+                    return $ Env r' $ Comma as'
+                _ | a == a' -> return a
+                _           -> evalTillCons r a')
 
 
         detachRef :: Ref -> Expr -> (Ref, Expr)
@@ -287,7 +334,7 @@ evalStep r e = case e of
         ref r e = fst $ detachRef r e
 
 
-        evalTillLambda :: Evaluator
+        evalTillLambda :: Evaluator Expr Expr
         evalTillLambda r a = let (r', b) = detachRef r a in case b of
             Var _ -> do
                 a' <- evalStep r a
